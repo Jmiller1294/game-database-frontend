2d729783819128dc11a54567167fe0e7990e2a0f
Jmiller1294 <jayman1294@gmail.com>
2021-01-04 02:19:52 -0500

    added game.css file

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/Game.css'
 
 const Game = (props) => {
   return (
-    <div>
+    <div className="game-container">
       <p>{props.game.name}</p>
       {
-        props.game.cover === undefined ? <p>No Image</p> : <img src={props.game.cover.url} alt="Cover Art"/>
+        props.game.cover === undefined ? <p>No Image</p> : <img className="cover-art" src={props.game.cover.url} alt="Cover Art"/>
       }  
     </div>
   )
