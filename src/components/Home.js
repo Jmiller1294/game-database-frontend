565c6dac044b0f3c423d9d8666a25826fb3cdb2d
Jmiller1294 <jayman1294@gmail.com>
2021-01-09 05:08:34 -0500

    added gamespot api

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,26 @@ import background from '../assets/homepage-background.jpg'
 
 class Home extends Component {
 
+
+    
+    
+
+    componentDidMount() {
+        const proxy = "http://127.0.0.1:8080/"
+        const url = "https://www.gamespot.com/api/articles/?api_key=50aaadecc9866a73d5679c6e5c6a3ce3ffa5510b&format=json&limit=5"
+        fetch(proxy + url, {
+            method  : 'GET', 
+            headers : {
+            "Accept"       : "application/json",
+            "Content-Type" : "application/json",
+             "User-Agent"   : "Justin"
+            }
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(error => console.log('error', error));
+    }
+
     handleLogout() {
         this.props.logout()
         this.props.history.push("/login");
