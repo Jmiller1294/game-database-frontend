6b256a63676031ea98fd65eb0d739e843536e6f3
Jmiller1294 <jayman1294@gmail.com>
2021-02-02 22:47:22 -0500

    made changes to css and added jsx to info component

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ class Home extends Component {
         this.props.history.push("/login");
     }
 
-    componentDidMount() {
+    getArticles() {
         const url = 'http://newsapi.org/v2/everything?q=games' +
         '&apiKey=0176803c14204800ae658c2d02a9c37f';
         fetch(url) 
@@ -25,7 +25,30 @@ class Home extends Component {
             articles: data.articles
         }))
     }
+    
+    getGames() {
+        const proxy = "http://127.0.0.1:8080/"
+        const url = "https://api.igdb.com/v4/games"
+        fetch(proxy + url, {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Client-ID': 'yy5am1hpn894dvf7mqk3k1ifx4qfkz',
+            'Authorization': 'Bearer mkvho4b2s24a2o6ack23l52lfj9t0r',
+        },
+        body: `fields name,cover.*;where first_release_date > ${Math.floor(Date.now() / 1000)};limit 20;`
+        })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+        .catch(error => {console.log('error', error)})
+    }
+    
+    componentDidMount() {
+        this.getGames()
+        this.getArticles()
+    }
 
+   
     render() {
         console.log(this.props)
             return(
--- src/components/Info.js
+++ src/components/Info.js
@@ -6,12 +6,23 @@ const Info = (props) => {
         {console.log(props.location)}
         {props.location.game ?
           <div className="info-container">
-            <h1>{props.location.game.name}</h1>
+            <h1 className="game-title">{props.location.game.name}</h1>
             <img className="cover" src={props.location.game.cover.url} alt="cover"></img>
-            <div>
-            {props.location.game.videos.map(video => <iframe title={video.name} src={`https://www.youtube.com/embed/${video.video_id}`} width="400" height="300" ></iframe>) }
+            <p className="game-rating">Game Rating: {Math.floor(props.location.game.rating)}/100</p>
+            <h3>Platforms</h3>
+            <ul className="platform-list">{props.location.game.platforms.map(platform => <li key={platform.id}>{platform.name}</li>)}</ul>
+            <h3>Videos</h3>
+            <div className="video-container">
+            {props.location.game.videos.slice(0,4).map(video => <iframe className="" key={video.id} title={video.name} src={`https://www.youtube.com/embed/${video.video_id}`} width="360" height="300" ></iframe>) }
             </div>
-            
+            <h3>Screenshots</h3>
+            <div className="screenshot-container">
+              {props.location.game.screenshots.map(screenshot => <img className="screenshot" src={screenshot.url} alt="screenshot"></img>)}
+            </div> 
+            <h3>Similar Games</h3>
+            <div className="similar-games-container">
+              {props.location.game.similar_games.map(game => <img className="similar-game" src={game.cover.url} alt="similar games"></img>)}
+            </div>
           </div>
         :
           <h1>No Game Found</h1>
