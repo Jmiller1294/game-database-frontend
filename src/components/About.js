6921cd0241ce9d7bdbab719e452e63447d7d60f6
Jmiller1294 <jayman1294@gmail.com>
2021-02-12 00:38:43 -0500

    made changes to ConsoleInfo and About components

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,13 @@ import React from 'react';
 const About = () => {
   return(
     <div>
-      About
+      <p>
+        Game Database is an application created by 
+        Justin Miller in 2021 that allows users to 
+        access information on thousands of games 
+        and consoles. It was created to give users 
+        one place to get info on old and recent games.
+      </p>
     </div>
   )
 }
--- src/components/ConsoleInfo.js
+++ src/components/ConsoleInfo.js
@@ -3,10 +3,17 @@ import React from 'react';
 const ConsoleInfo = (props) => {
 
   return(
-    <>
+    <div className="console-info-container">
       {console.log(props.location.console)}
-      {props.location.company}
-    </>
+      <h3>{props.location.console.name}</h3>
+      <h4>Release Date: {props.location.console.release_date}</h4>
+      <h4>Company: {props.location.console.company}</h4>
+      <h4>Generation: {props.location.console.generation}</h4>
+      <h4>Launch Date: {props.location.console.launch_price}</h4>
+      <h4>CPU: {props.location.console.cpu}</h4>
+      <h4>GPU: {props.location.console.gpu}</h4>
+      <h4>Units Sold: {props.location.console.units_sold}</h4>
+    </div>
   )
 }
 export default ConsoleInfo;
