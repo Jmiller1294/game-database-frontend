a8e5e18346cbca99c465d75214b230f4c7b6f920
Jmiller1294 <jayman1294@gmail.com>
2021-03-06 03:35:07 -0500

    added styled components

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import '../styles/About.css'
-import photo from '../assets/personal-photo.jpg'
+import '../styles/About.css';
+import photo from '../assets/personal-photo.jpg';
+import styled from 'styled-components';
+
+const Description = styled.p`
+  color:  white;
+`
 
 const About = () => {
   return(
     <div className="about-container">
-      <img src={photo} alt="about picture"></img>
-      <p className="description">
+      <img src={photo} alt="about"></img>
+      <Description className="description">
         Game Database is an application created by 
         Justin Miller in 2021 that allows users to 
         access information on thousands of games 
         and consoles. It was created to give users 
         one place to get info on old and recent games.
-      </p>
+      </Description>
     </div>
   )
 }
