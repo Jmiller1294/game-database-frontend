ce5001e2a7235afc7ed1eb81603a14a05a459a22
Jmiller1294 <jayman1294@gmail.com>
2021-04-26 23:56:56 -0400

    replaced Navbar.css with styled components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import '../styles/NavBar.css'
+import styled from 'styled-components';
 
+export const Row = styled.div`
+   display: flex;
+   background-color: #050505;
+   height: 42px;
+`;
+
+export const NavItem = styled.li`
+    
+`
 
 const NavBar = () => {
     return (
-       <ul id="navbar">
+       <Row>
            <li className="site-name"><a href="/">Game Database</a></li>
            <li className="nav-item"></li>
            <li className="nav-item"><a href="/games">Games</a></li>
            <li className="nav-item"><a href="/consoles">Consoles</a></li>
            <li className="nav-item about-button"><a href="/about">About</a></li>
            <li className="nav-item login-button"><a href="/login">Login</a></li>
-       </ul>
+       </Row>
     )
 }
 export default NavBar;
